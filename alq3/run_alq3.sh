549f50e89267ef089620b9fda22fadb56d599163
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2012-09-05 20:02:00 +0200

    kmcsingle options added

--- alq3/run_alq3.sh
+++ alq3/run_alq3.sh
@@ -6,6 +6,6 @@ ctp_run -e "rates" -o options.xml -f state_alq3.sql
 ctp_run -e "eanalyze ianalyze density" -o options.xml -f state_alq3.sql
 
 #ctp_run -e "eimport iimport stateserver pairdump xmultipole" -o options.xml -f state_alq3.sql
-#kmc_run -e "kmcparallel" -o options.xml -f state_alq3.sql
+kmc_run -e "kmcparallel" -o options.xml -f state_alq3.sql
 
 
