10e537664e6d3d4bdc669ba6325aecdea648277e
Carl Poelking <carl.poelking@mpip-mainz.mpg.de>
2013-04-15 14:14:31 +0200

    Edited run.sh

--- alq3/run_alq3.sh
+++ alq3/run_alq3.sh
@@ -1,11 +1,9 @@
-
 ctp_map -t MD_FILES/system.tpr -c MD_FILES/system.gro -s system.xml -f state_alq3.sql
 
 ctp_run -e "tdump einternal emultipole neighborlist eoutersphere izindo" -o options.xml -f state_alq3.sql -t 4
 ctp_run -e "rates" -o options.xml -f state_alq3.sql
 ctp_run -e "eanalyze ianalyze density" -o options.xml -f state_alq3.sql
 
-#ctp_run -e "eimport iimport stateserver pairdump xmultipole" -o options.xml -f state_alq3.sql
-kmc_run -e "kmcparallel" -o options.xml -f state_alq3.sql
+kmc_run -e "kmcsingle" -o options.xml -f state_alq3.sql
 
 
