66d0d8defec986a4b838e021bbefa21e28f52879
Pascal Kordt <kordt@mpip-mainz.mpg.de>
2013-10-14 14:52:56 +0200

    run_alq3.sh updated

--- alq3/run_alq3.sh
+++ alq3/run_alq3.sh
@@ -1,9 +1,19 @@
+# Mapping
 ctp_map -t MD_FILES/system.tpr -c MD_FILES/system.gro -s system.xml -f state_alq3.sql
 
-ctp_run -e "tdump einternal emultipole neighborlist eoutersphere izindo" -o options.xml -f state_alq3.sql -t 4
-ctp_run -e "rates" -o options.xml -f state_alq3.sql
-ctp_run -e "eanalyze ianalyze density" -o options.xml -f state_alq3.sql
+# Neighbor list
+ctp_run -e neighborlist -o options.xml -f state_alq3.sql -t 4
 
-kmc_run -e "kmcsingle" -o options.xml -f state_alq3.sql
+# Site energies
+ctp_run -e emultipole -o options.xml -f state_alq3.sql -t 4
+ctp_run -e einternal -o options.xml -f state_alq3.sql -t 4
+ctp_run -e eoutersphere -o options.xml -f state_alq3.sql -t 4
 
+# Transfer integrals
+ctp_run -e izindo -o options.xml -f state_alq3.sql -t 4
 
+# Rates
+ctp_run -e rates -o options.xml -f -t 4
+
+# Charge Transport
+kmc_run -e kmcmultiple -o options.xml -f state_alq3.sql state_alq3.sql
